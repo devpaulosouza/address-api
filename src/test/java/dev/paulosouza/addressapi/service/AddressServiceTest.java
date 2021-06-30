a18db9fe1d5783e4b681ba6fa210eeb240524df4
Paulo <paulosouza.ti@gmail.com>
2021-06-30 09:38:59 -0300

    imposrts removidos

--- src/test/java/dev/paulosouza/addressapi/service/AddressServiceTest.java
+++ src/test/java/dev/paulosouza/addressapi/service/AddressServiceTest.java
@@ -10,17 +10,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.support.PagedListHolder;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageImpl;
-import org.springframework.data.domain.Pageable;
 
-import java.util.Collections;
-import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 
 
